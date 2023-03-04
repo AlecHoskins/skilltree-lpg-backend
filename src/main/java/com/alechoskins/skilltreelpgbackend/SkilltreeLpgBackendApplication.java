3f88392eb1bb6ee85db925b4e99a885859418a6a
abhos <alecbhoskins@gmail.com>
2023-03-04 14:02:02 -0600

    initial commit

---
+++ src/main/java/com/alechoskins/skilltreelpgbackend/SkilltreeLpgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alechoskins.skilltreelpgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilltreeLpgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilltreeLpgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alechoskins/skilltreelpgbackend/SkilltreeLpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alechoskins.skilltreelpgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilltreeLpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
